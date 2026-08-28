f2f5b222ed3f7133b9ca2e8e4c593ae283537dea
apichart050836-beep <apichart.c@cmu.ac.th>
2026-08-28 12:08:24 +0700

    update

---
+++ git-push.bat
@@ -0,0 +1,20 @@
+@echo off
+cd /d "%~dp0"
+
+set /p msg=กรอกข้อความอธิบายสิ่งที่แก้ (Enter เพื่อใช้ค่าเริ่มต้น "update"):
+if "%msg%"=="" set msg=update
+
+echo.
+echo === git add -A ===
+git add -A
+
+echo.
+echo === git commit -m "%msg%" ===
+git commit -m "%msg%"
+
+echo.
+echo === git push ===
+git push
+
+echo.
+pause
